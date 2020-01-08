c56e764edcfc2e07a574df3472c3607a8d5fe1b7
Dominik Engel <DE@nea-x.net>
2020-01-08 14:37:51 +0100

    Server side pagination
    THis adds the server side pagiantion to the component. If count of Tablepagiantion is -1, the pagiantion text changes to X of more than Y and the nextPage button is kept enabled.

--- packages/material-ui/src/TablePagination/TablePagination.test.js
+++ packages/material-ui/src/TablePagination/TablePagination.test.js
@@ -246,6 +246,35 @@ describe('<TablePagination />', () => {
       assert.strictEqual(wrapper.text().indexOf('Rows per page'), -1);
       assert.strictEqual(wrapper.find(Select).length, 0);
     });
+
+    it('should display the "of more than" text and keep the nextButton enabled, if count is -1 ', () => {
+      const wrapper = mount(
+        <table>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                page={0}
+                rowsPerPage={5}
+                rowsPerPageOptions={[5]}
+                onChangePage={noop}
+                onChangeRowsPerPage={noop}
+                count={-1}
+              />
+            </TableRow>
+          </TableFooter>
+        </table>,
+      );
+
+      assert.strictEqual(
+        wrapper
+          .find(Typography)
+          .at(0)
+          .text(),
+        '1-5 of more than 5',
+      );      
+      const nextButton = wrapper.find(IconButton).at(1);
+      assert.strictEqual(nextButton.props().disabled, false);
+    });
   });
 
   describe('warnings', () => {
