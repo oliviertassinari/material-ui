b060994bb442223316ff964b65039e2838cca994
Olivier Tassinari <olivier.tassinari@gmail.com>
2020-09-25 21:28:30 +0200

    Improve reading experience

--- docs/src/pages/components/tooltips/AccessibilityTooltips.tsx
+++ docs/src/pages/components/tooltips/AccessibilityTooltips.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
-import DeleteIcon from '@material-ui/icons/Delete';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
 
 export default function AccessibilityTooltips() {
   return (
     <div>
       <Tooltip title="Delete">
-        <IconButton>
-          <DeleteIcon />
-        </IconButton>
+        <Button>Delete</Button>
       </Tooltip>
-      <Tooltip describeChild title="does not add if it already exists">
+      <Tooltip describeChild title="Does not add if it already exists">
         <Button>Add</Button>
       </Tooltip>
     </div>
