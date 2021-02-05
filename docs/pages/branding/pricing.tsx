34df15daed320ea71195d26755889a99b42b662b
Matt <github@nospam.33m.co>
2021-02-05 23:14:25 +0000

    use BrandingRoot wrapper

--- docs/pages/branding/pricing.tsx
+++ docs/pages/branding/pricing.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
+import BrandingRoot from 'docs/src/modules/branding/BrandingRoot';
 import BrandingPrice from 'docs/src/modules/branding/BrandingPrice';
 
+
 export default function Page() {
-  return <BrandingPrice />;
+  return (
+    <BrandingRoot>
+      <BrandingPrice />
+    </BrandingRoot>
+  );
 }
