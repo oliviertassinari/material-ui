89d2355e5a297e42f212ffc7abe0fb407f123665
Matt <github@nospam.33m.co>
2021-02-05 23:13:31 +0000

    yarn prettier

--- docs/pages/branding/pricing.tsx
+++ docs/pages/branding/pricing.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import BrandingPrice from 'docs/src/modules/branding/BrandingPrice';
 
-
 export default function Page() {
-  return (
-      <BrandingPrice/>
-  );
+  return <BrandingPrice />;
 }
